2904c933275781a16360dc6f24407f74751f8a59
machida <machida@fjord.jp>
2011-09-02 15:44:00 +0900

    changed season

--- app.rb
+++ app.rb
@@ -34,9 +34,9 @@ helpers do
     case Time.now.month
     when 3..5
       'spring'
-    when 6..8
+    when 6..9
       'summer'
-    when 9..10
+    when 10
       'autumn'
     else
       'winter'
